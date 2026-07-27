84b2269146857f12a476d5da8ae7245f23c562b6
yu-mi-n <igawa.yuito0818@gmail.com>
2026-07-27 12:03:21 +0900

    2026/07/26 heart追加

--- src/test/Test01.java
+++ src/test/Test01.java
@@ -1,7 +1,12 @@
 package test;
 
+import java.util.Scanner;
+
 public class Test01 {
 	public static void main(String[] args) {
-		System.out.println("内田りょう");
+		System.out.println("内田りょう♡");
+		System.out.println("syumai");
+		Scanner scan = new Scanner(System.in);
+		scan.close();
 	}
 }
