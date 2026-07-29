3a984b031aede6a228a05bcf575918dd97d48952
syumaiking <noguchisyudai0310@gmail.com>
2026-07-29 13:44:00 +0900

    カプセル化に対応させました！

--- src/test/ShowInfo.java
+++ src/test/ShowInfo.java
@@ -1,11 +1,11 @@
 package test;
 
 public class ShowInfo {
-	void showInfo() {
+	public showInfo(MyName account) {
 		System.out.println("------------------");
-		System.out.println("名前:" + name);
-		System.out.println("年齢:" + age);
-		System.out.println("性格" +　personality);
+		System.out.println("名前:" + account.getName());
+		System.out.println("年齢:" + account.getAge());
+		System.out.println("性格" + account.getPersonality());
 		System.out.println("------------------");
 	}
 }
