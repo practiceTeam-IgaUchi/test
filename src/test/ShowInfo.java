8c6644f88ec46c2a5efc4e4e174dc242c773d958
syumaiking <noguchisyudai0310@gmail.com>
2026-07-29 13:39:10 +0900

    ShowInfoを追加しました。

---
+++ src/test/ShowInfo.java
@@ -0,0 +1,11 @@
+package test;
+
+public class ShowInfo {
+	void showInfo() {
+		System.out.println("------------------");
+		System.out.println("名前:" + name);
+		System.out.println("年齢:" + age);
+		System.out.println("性格" +　personality);
+		System.out.println("------------------");
+	}
+}
