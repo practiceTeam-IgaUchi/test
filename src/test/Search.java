6133848e18e90ff7aa8661038012736e226cc81e
delfineorigin <delfineorigin@10.95.2.29>
2026-07-29 14:17:53 +0900

    Search追加しました

--- src/test/Search.java
+++ src/test/Search.java
@@ -1,5 +1,23 @@
 package test;
 
+import java.util.List;
+import java.util.Scanner;
+
 public class Search {
 
+	public int searchIndex(List<MyName> data) {
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.println("一覧:");
+		for (int i = 0; i < data.length; i++) {
+			System.out.println(i + " : " + data[i].getName());
+		}
+
+		System.out.print("選んでください: ");
+		int index = scanner.nextInt();
+
+		scanner.close();
+
+		return index;
+	}
 }
